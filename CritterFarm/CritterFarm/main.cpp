3238e1d765b64b6bd4e180b7e0c7c8b72da27464
Shiner89 <43843479+Shiner89@users.noreply.github.com>
2018-10-04 10:14:14 +0100

    finished
    tutorial finished

--- CritterFarm/CritterFarm/main.cpp
+++ CritterFarm/CritterFarm/main.cpp
@@ -10,12 +10,20 @@ int main()
 	cout << "My critter's name is " << crit.GetName() << endl;
 
 	cout << "\nCreating critter farm.\n";
-	Farm myFarm(3);
+	Farm myFarm(6);
+	string name;
 
-	cout << "\nAdding three critters to the farm.\n";
+	cout << "\nAdding 6 critters to the farm.\n";
 	myFarm.Add(Critter("Moe"));
 	myFarm.Add(Critter("Larry"));
 	myFarm.Add(Critter("Curly"));
+	myFarm.Add(Critter("Ringo"));
+	myFarm.Add(Critter("Paul"));
+	cout << "Please enter final name for critter?\n";
+	cin >> name;
+	myFarm.Add(Critter(name));
+	cin.get();
+
 
 	cout << "\nCalling Roll...\n";
 	myFarm.RollCall();
